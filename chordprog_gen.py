057dde0df410c5ded734d94fb588d277afe5f2a1
hng201 <55842986+hng201@users.noreply.github.com>
2020-08-21 22:44:00 +0100

    Implemented C Major chord progression notes

--- chordprog_gen.py
+++ chordprog_gen.py
@@ -1,4 +1,5 @@
 import random
+import music21
 
 # Array to store generated chord progression
 new_chord_progression = []
@@ -75,7 +76,7 @@ def generate_chord_progression():
         # If chord at position x is 'vii'
         if new_chord_progression[x] == 'vii':
             # Generate random number to determine next chord
-            num = random.randint(0,1)
+            num = random.randint(0, 1)
             if num == 0:
                 # Add chord 'i' as the next chord
                 new_chord_progression.append('i')
@@ -86,11 +87,76 @@ def generate_chord_progression():
                 x += 1
     # Display the generated chord progression
     print(new_chord_progression)
+    # Add C Major chord progression notes to the generated chord progression
+    cmaj_chord_progression(new_chord_progression)
 
 
+# Adds C Major chord progression notes
+def cmaj_chord_progression(chord_progression_structure):
+    # Create new stream to store chord progression notes
+    chord_prog_stream = music21.stream.Stream()
+    # Variable to store bass clef value
+    bass_clef = music21.clef.BassClef()
+    # Add the base clef to the chord progression stream
+    chord_prog_stream.append(bass_clef)
 
-
-
-
-
-
+    # For each chord in the chord progression structure
+    for x in chord_progression_structure:
+        # If chord = 'i'
+        if x == 'i':
+            # Create new chord and assign C major triad notes
+            c_maj_chord = music21.chord.Chord(["C3", "E3", "G3"])
+            # Set chord duration to semibreve/whole
+            c_maj_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(c_maj_chord)
+        # If chord = 'ii'
+        if x == 'ii':
+            # Create new chord and assign D minor triad notes
+            d_min_chord = music21.chord.Chord(["D3", "F3", "A3"])
+            # Set chord duration to semibreve/whole
+            d_min_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(d_min_chord)
+        # If chord = 'iii'
+        if x == 'iii':
+            # Create new chord and assign E minor triad notes
+            e_min_chord = music21.chord.Chord(["E3", "G3", "B3"])
+            # Set chord duration to semibreve/whole
+            e_min_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(e_min_chord)
+        # If chord = 'iv'
+        if x == 'iv':
+            # Create new chord and assign F major triad notes
+            f_maj_chord = music21.chord.Chord(["F2", "A2", "C3"])
+            # Set chord duration to semibreve/whole
+            f_maj_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(f_maj_chord)
+        # If chord = 'v'
+        if x == 'v':
+            # Create new chord and assign G major triad notes
+            g_maj_chord = music21.chord.Chord(["G2", "B2", "D3"])
+            # Set chord duration to semibreve/whole
+            g_maj_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(g_maj_chord)
+        # If chord = 'vi'
+        if x == 'vi':
+            # Create new chord and assign A minor triad notes
+            a_min_chord = music21.chord.Chord(["A2", "C3", "E3"])
+            # Set chord duration to semibreve/whole
+            a_min_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(a_min_chord)
+        # If chord = 'vii'
+        if x == 'vii':
+            # Create new chord and assign B diminished triad notes
+            b_dim_chord = music21.chord.Chord(["B2", "D3", "F3"])
+            # Set chord duration to semibreve/whole
+            b_dim_chord.duration.type = "whole"
+            # Add chord to stream
+            chord_prog_stream.append(b_dim_chord)
+    # Display chord progression in MusicXML format
+    chord_prog_stream.show()
