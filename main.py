125e32c5e83f41abfd942d504358b756e5709790
hng201 <55842986+hng201@users.noreply.github.com>
2020-09-05 22:42:57 +0100

    Added comments and removed redundant chord_progression variable initialisation

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ us = music21.environment.UserSettings()
 us['musicxmlPath'] = 'C:/Program Files/MuseScore 3/bin/MuseScore3.exe'
 
 if __name__ == '__main__':
+    # Create a new stream
     stream = music21.stream.Stream()
-    chord_progression = []
+    # Generate a chord progression structure and assign the value to variable chord_progression
     chord_progression = chordprog_gen.generate_chord_progression()
+    # Generate Cmaj chord progression and assign the value to variable p_accomp
     p_accomp = chordprog_gen.cmaj_chord_progression(chord_progression)
+    # Generate a melody and assign the value to variable p_melody
     p_melody = melody_gen.generate_melody(chord_progression)
+    # Add the melody part to the stream
     stream.append(p_melody)
+    # Add the accompaniment part to the stream
     stream.append(p_accomp)
+    # Show the stream in MusicXML format
     stream.show()
 
