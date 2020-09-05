eab159eecdd12b6f510f9fb1e929b83fac75f7ed
hng201 <55842986+hng201@users.noreply.github.com>
2020-09-05 22:35:05 +0100

    Added code to test melody_gen and to combine melody and accomp together

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-import music21
-import chordprog_gen
+import music21, chordprog_gen, melody_gen
 
 us = music21.environment.UserSettings()
 # Set path for software to open music scores
 us['musicxmlPath'] = 'C:/Program Files/MuseScore 3/bin/MuseScore3.exe'
 
 if __name__ == '__main__':
-    chordprog_gen.generate_chord_progression()
+    stream = music21.stream.Stream()
+    chord_progression = []
+    chord_progression = chordprog_gen.generate_chord_progression()
+    p_accomp = chordprog_gen.cmaj_chord_progression(chord_progression)
+    p_melody = melody_gen.generate_melody(chord_progression)
+    stream.append(p_melody)
+    stream.append(p_accomp)
+    stream.show()
 
