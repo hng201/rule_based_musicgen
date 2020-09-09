b7df59457651b243214573e0d95b085c0bce368c
hng201 <55842986+hng201@users.noreply.github.com>
2020-09-09 21:46:22 +0100

    Implemented passing note

--- melody_gen.py
+++ melody_gen.py
@@ -22,13 +22,22 @@ def choose_note(chord):
     current_note = ""
     # Chooses 4 crochet notes
     for y in range(0, 4):
-        # Call method to select a triad note
-        current_note = select_triad_note(chord)
+        if current_note == '':
+            # Call method to select a triad note
+            current_note = select_triad_note(chord)
+        else:
+            # Generate a random number between 0 and 1 to decide whether next note is a triad note or a passing note
+            num = random.randint(0, 1)
+            if num == 0:
+                # Call method to select a triad note
+                current_note = select_triad_note(chord)
+            else:
+                # Call method to select a passing note
+                select_passing_note(chord, current_note)
 
 
 def select_triad_note(chord):
     # Variable to store the selected note
-    # May be used later for passing note selection
     note = ""
     # Variable to store randomly generated number within range of 0 and 2
     num = random.randint(0, 2)
@@ -146,3 +155,138 @@ def select_triad_note(chord):
             note = "F"
     # Return the value of note
     return note
+
+def select_passing_note(chord, note):
+    if chord == 'i':
+        # If the note is C
+        if note == 'C':
+            # Add the passing note D to the melody stream
+            melody_stream.append(music21.note.Note("D"))
+        # If the note is E
+        if note == 'E':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note F to the melody stream
+                melody_stream.append(music21.note.Note("F"))
+            else:
+                # Add the passing note D to the melody stream
+                melody_stream.append(music21.note.Note("D"))
+        # If the note is G
+        if note == 'G':
+            # Add the passing note F to the melody stream
+            melody_stream.append(music21.note.Note("F"))
+    if chord == 'ii':
+        # If the note is D
+        if note == 'D':
+            # Add the passing note E to the melody stream
+            melody_stream.append(music21.note.Note("E"))
+        # If the note is F
+        if note == 'F':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note G to the melody stream
+                melody_stream.append(music21.note.Note("G"))
+            else:
+                # Add the passing note E to the melody stream
+                melody_stream.append(music21.note.Note("E"))
+        # If the note is A
+        if note == 'A':
+            # Add the passing note G to the melody stream
+            melody_stream.append(music21.note.Note("G"))
+    if chord == 'iii':
+        # If the note is E
+        if note == 'E':
+            # Add the passing note F# to the melody stream
+            melody_stream.append(music21.note.Note("F#"))
+        # If the note is G
+        if note == 'G':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note A to the melody stream
+                melody_stream.append(music21.note.Note("A"))
+            else:
+                # Add the passing note F# to the melody stream
+                melody_stream.append(music21.note.Note("F#"))
+        # If the note is B
+        if note == 'B':
+            # Add the passing note F to the melody stream
+            melody_stream.append(music21.note.Note("F"))
+    if chord == 'iv':
+        # If the note is F
+        if note == 'F':
+            # Add the passing note G to the melody stream
+            melody_stream.append(music21.note.Note("G"))
+        # If the note is A
+        if note == 'A':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note Bb to the melody stream
+                melody_stream.append(music21.note.Note("B-"))
+            else:
+                # Add the passing note G to the melody stream
+                melody_stream.append(music21.note.Note("G"))
+        # If the note is C
+        if note == 'C':
+            # Add the passing note Bb to the melody stream
+            melody_stream.append(music21.note.Note("B-"))
+    if chord == 'v':
+        # If the note is G
+        if note == 'G':
+            # Add the passing note A to the melody stream
+            melody_stream.append(music21.note.Note("A"))
+        # If the note is B
+        if note == 'B':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note C to the melody stream
+                melody_stream.append(music21.note.Note("C"))
+            else:
+                # Add the passing note A to the melody stream
+                melody_stream.append(music21.note.Note("A"))
+        # If the note is D
+        if note == 'D':
+            # Add the passing note C to the melody stream
+            melody_stream.append(music21.note.Note("C"))
+    if chord == 'vi':
+        # If the note is A
+        if note == 'A':
+            # Add the passing note B to the melody stream
+            melody_stream.append(music21.note.Note("B"))
+        # If the note is C
+        if note == 'C':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the note D to the melody stream
+                melody_stream.append(music21.note.Note("D"))
+            else:
+                # Add the note B to the melody stream
+                melody_stream.append(music21.note.Note("B"))
+        # If the note is E
+        if note == 'E':
+            # Add the note D to the melody stream
+            melody_stream.append(music21.note.Note("D"))
+    if chord == 'vii':
+        # If the note is B
+        if note == 'B':
+            # Add the note C# to the melody stream
+            melody_stream.append(music21.note.Note("C#"))
+        # If the note is D
+        if note == 'D':
+            # Generate a random number between 0 and 1 to decide which passing note to use
+            num = random.randint(0, 1)
+            if num == 0:
+                # Add the passing note E to the melody stream
+                melody_stream.append(music21.note.Note("E"))
+            else:
+                # Add the passing note C# to the melody stream
+                melody_stream.append(music21.note.Note("C#"))
+        # If the note is F
+        if note == 'F':
+            # Add the passing note E to the melody stream
+            melody_stream.append(music21.note.Note("E"))
