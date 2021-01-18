5127201bd53b470954a68b4e467a4da641f58299
hng201 <55842986+hng201@users.noreply.github.com>
2021-01-18 20:25:14 +0000

    Implemented e minor basic chord accomp notes and added key signature

--- accomp_gen.py
+++ accomp_gen.py
@@ -21,6 +21,7 @@ c_major_chord_progression = [c_maj_chord, d_min_chord, e_min_chord, f_maj_chord,
 g_major_chord_progression = [g_maj_chord, a_min_chord, b_min_chord, c_maj_chord, d_maj_chord, e_min_chord, fsharp_dim_chord]
 
 a_minor_chord_progression = [a_min_chord, b_dim_chord, c_maj_chord, d_min_chord, e_min_chord, f_maj_chord, g_maj_chord]
+e_minor_chord_progression = [e_min_chord, fsharp_dim_chord, g_maj_chord, a_min_chord, b_min_chord, c_maj_chord, d_maj_chord]
 
 accomp_stream = music21.stream.Part()
 bass_clef = music21.clef.BassClef()
@@ -43,10 +44,19 @@ def generate_chord_accompaniment(chord_progression, major):
             chord_key = c_major_chord_progression
             print("Key: C Major")
         else:
+            key_signature = music21.key.KeySignature(1)
+            accomp_stream.append(key_signature)
             chord_key = g_major_chord_progression
             print("Key: G Major")
     # Select from minor keys
     else:
-        chord_key = a_minor_chord_progression
+        if x == 0:
+            chord_key = a_minor_chord_progression
+            print("Key: A Minor")
+        else:
+            key_signature = music21.key.KeySignature(1)
+            accomp_stream.append(key_signature)
+            chord_key = e_minor_chord_progression
+            print("Key: E Minor")
     for chord in chord_progression:
         accomp_stream.append(music21.chord.Chord(chord_key[chord - 1]))
