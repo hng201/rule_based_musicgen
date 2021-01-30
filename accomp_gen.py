85c8eae0ee13012127669d16fc1e56cf10c2f4dd
hng201 <55842986+hng201@users.noreply.github.com>
2021-01-30 21:42:40 +0000

    Renamed variable

--- accomp_gen.py
+++ accomp_gen.py
@@ -25,14 +25,20 @@ b_min_chord = ["B2", "D3", "F#3"]
 b_dim_chord = ["B2", "D3", "F3"]
 
 c_major_chord_progression = [c_maj_chord, d_min_chord, e_min_chord, f_maj_chord, g_maj_chord, a_min_chord, b_dim_chord]
-g_major_chord_progression = [g_maj_chord, a_min_chord, b_min_chord, c_maj_chord, d_maj_chord, e_min_chord, fsharp_dim_chord]
-d_major_chord_progression = [d_maj_chord, e_min_chord, fsharp_min_chord, g_maj_chord, a_maj_chord, b_min_chord, csharp_dim_chord]
-a_major_chord_progression = [a_maj_chord, b_min_chord, csharp_min_chord, d_maj_chord, e_maj_chord, fsharp_min_chord, gsharp_dim_chord]
+g_major_chord_progression = [g_maj_chord, a_min_chord, b_min_chord, c_maj_chord, d_maj_chord, e_min_chord,
+                             fsharp_dim_chord]
+d_major_chord_progression = [d_maj_chord, e_min_chord, fsharp_min_chord, g_maj_chord, a_maj_chord, b_min_chord,
+                             csharp_dim_chord]
+a_major_chord_progression = [a_maj_chord, b_min_chord, csharp_min_chord, d_maj_chord, e_maj_chord, fsharp_min_chord,
+                             gsharp_dim_chord]
 
 a_minor_chord_progression = [a_min_chord, b_dim_chord, c_maj_chord, d_min_chord, e_min_chord, f_maj_chord, g_maj_chord]
-e_minor_chord_progression = [e_min_chord, fsharp_dim_chord, g_maj_chord, a_min_chord, b_min_chord, c_maj_chord, d_maj_chord]
-b_minor_chord_progression = [b_min_chord, csharp_dim_chord, d_maj_chord, e_min_chord, fsharp_min_chord, g_maj_chord, a_maj_chord]
-fsharp_minor_chord_progression = [fsharp_min_chord, gsharp_dim_chord, a_maj_chord, b_min_chord, csharp_min_chord, d_maj_chord, e_maj_chord]
+e_minor_chord_progression = [e_min_chord, fsharp_dim_chord, g_maj_chord, a_min_chord, b_min_chord, c_maj_chord,
+                             d_maj_chord]
+b_minor_chord_progression = [b_min_chord, csharp_dim_chord, d_maj_chord, e_min_chord, fsharp_min_chord, g_maj_chord,
+                             a_maj_chord]
+fsharp_minor_chord_progression = [fsharp_min_chord, gsharp_dim_chord, a_maj_chord, b_min_chord, csharp_min_chord,
+                                  d_maj_chord, e_maj_chord]
 
 accomp_stream = music21.stream.Part()
 bass_clef = music21.clef.BassClef()
@@ -40,25 +46,25 @@ accomp_stream.append(bass_clef)
 
 
 def generate_accompaniment(chord_progression, major, note_duration):
-    generate_chord_accompaniment(chord_progression, major, note_duration)
-    return accomp_stream
+    key = generate_chord_accompaniment(chord_progression, major, note_duration)
+    return accomp_stream, key
 
 
 def generate_chord_accompaniment(chord_progression, major, note_duration):
     # Random int to decide key
     # For testing limited to number of keys implemented currently
-    x = random.randint(0, 3)
+    key = random.randint(0, 3)
     # If it is a major chord progression
     if major == True:
-        if x == 0:
+        if key == 0:
             chord_key = c_major_chord_progression
             print("Key: C Major")
-        elif x == 1:
+        elif key == 1:
             key_signature = music21.key.KeySignature(1)
             accomp_stream.append(key_signature)
             chord_key = g_major_chord_progression
             print("Key: G Major")
-        elif x == 2:
+        elif key == 2:
             key_signature = music21.key.KeySignature(2)
             accomp_stream.append(key_signature)
             chord_key = d_major_chord_progression
@@ -70,15 +76,15 @@ def generate_chord_accompaniment(chord_progression, major, note_duration):
             print("Key: A Major")
     # Select from minor keys
     else:
-        if x == 0:
+        if key == 0:
             chord_key = a_minor_chord_progression
             print("Key: A Minor")
-        elif x == 1:
+        elif key == 1:
             key_signature = music21.key.KeySignature(1)
             accomp_stream.append(key_signature)
             chord_key = e_minor_chord_progression
             print("Key: E Minor")
-        elif x == 2:
+        elif key == 2:
             key_signature = music21.key.KeySignature(2)
             accomp_stream.append(key_signature)
             chord_key = b_minor_chord_progression
@@ -105,4 +111,4 @@ def generate_chord_accompaniment(chord_progression, major, note_duration):
             x = x + note_duration[i].quarterLength
             # Increment position
             i += 1
-
+    return key
