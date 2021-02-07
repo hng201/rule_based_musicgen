4f0aed385e6f712ca36801e48dd487bc43968d6e
hng201 <55842986+hng201@users.noreply.github.com>
2021-02-07 18:48:23 +0000

    Implement G,D and A major notes and A, E, B, F# notes

--- melody_gen.py
+++ melody_gen.py
@@ -1,6 +1,15 @@
 import random, music21
 
 c_major_notes = ["C", "D", "E", "F", "G", "A", "B"]
+g_major_notes = ["G", "A", "B", "C", "D", "E", "F#"]
+d_major_notes = ["D", "E", "F#", "G", "A", "B", "C#"]
+a_major_notes = ["A", "B", "C#", "D", "E", "F#", "G#"]
+
+a_minor_notes = ["A", "B", "C", "D", "E", "F", "G"]
+e_minor_notes = ["E", "F#", "G", "A", "B", "C", "D"]
+b_minor_notes = ["B", "C#", "D", "E", "F#", "G", "A"]
+fsharp_minor_notes = ["F#", "G#", "A", "B", "C#", "D", "E"]
+
 
 melody_stream = music21.stream.Part()
 # Variable to store treble clef value
@@ -10,12 +19,42 @@ melody_stream.append(treble_clef)
 
 
 # Method to generate melody
-def generate_melody(chord_progression, melody_rhythm):
-    choose_note(chord_progression, melody_rhythm)
+def generate_melody(chord_progression, melody_rhythm, major, key):
+    if major:
+        if key == 0:
+            notes = c_major_notes
+        elif key == 1:
+            key_signature = music21.key.KeySignature(1)
+            melody_stream.append(key_signature)
+            notes = g_major_notes
+        elif key == 2:
+            key_signature = music21.key.KeySignature(2)
+            melody_stream.append(key_signature)
+            notes = d_major_notes
+        else:
+            key_signature = music21.key.KeySignature(3)
+            melody_stream.append(key_signature)
+            notes = a_major_notes
+    else:
+        if key == 0:
+            notes = a_minor_notes
+        elif key == 1:
+            key_signature = music21.key.KeySignature(1)
+            melody_stream.append(key_signature)
+            notes = e_minor_notes
+        elif key == 2:
+            key_signature = music21.key.KeySignature(2)
+            melody_stream.append(key_signature)
+            notes = b_minor_notes
+        else:
+            key_signature = music21.key.KeySignature(3)
+            melody_stream.append(key_signature)
+            notes = fsharp_minor_notes
+    choose_note(chord_progression, melody_rhythm, notes)
     return melody_stream
 
 
-def choose_note(chord_progression, melody_rhythm):
+def choose_note(chord_progression, melody_rhythm, notes):
     # Used to store current note value
     current_note = 0
     # Counter for rhythm
@@ -38,7 +77,7 @@ def choose_note(chord_progression, melody_rhythm):
                     # This is to ensure note value is not out of bound of array
                     note = note - 6
                 # Create a new note
-                new_note = music21.note.Note(c_major_notes[note])
+                new_note = music21.note.Note(notes[note])
                 # Assign the new note a duration
                 new_note.duration = melody_rhythm[i]
                 # Add note to melody stream
@@ -50,7 +89,7 @@ def choose_note(chord_progression, melody_rhythm):
                 # Increment position
                 i += 1
             # If the current note value is odd/ is a passing note
-            if current_note % 2 > 0:
+            elif current_note % 2 > 0:
                 # Select a note above or below the current note
                 note = random.choice([current_note - 1, current_note + 1])
                 # If note value is greater than 6
@@ -58,7 +97,7 @@ def choose_note(chord_progression, melody_rhythm):
                     # Minus 6 to get note value
                     note = note - 6
                 # Create new note
-                new_note = music21.note.Note(c_major_notes[note])
+                new_note = music21.note.Note(notes[note])
                 # Assign new note a duration
                 new_note.duration = melody_rhythm[i]
                 # Add new note to melody stream
@@ -77,7 +116,7 @@ def choose_note(chord_progression, melody_rhythm):
                     # Minus 6 to get note value
                     note = note - 6
                 # Create new note
-                new_note = music21.note.Note(c_major_notes[note])
+                new_note = music21.note.Note(notes[note])
                 # Assign new note a duration
                 new_note.duration = melody_rhythm[i]
                 # Add new note to melody stream
