60dd2abb231505eea4ff1e6b8b718e33568e78cb
hng201 <55842986+hng201@users.noreply.github.com>
2021-02-07 18:50:15 +0000

    Uncommented test code to add accompaniment

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ if __name__ == '__main__':
     stream = music21.stream.Stream()
     # Generate a chord progression structure and assign the value to variable chord_progression
     chord_progression, major = chordprog_gen.generate_chord_progression()
-    #note_duration = rhythm_gen.generate_note_duration(chord_progression)
+    accomp_rhythm = rhythm_gen.generate_note_duration(chord_progression)
     # Generate chord progression and assign the value to variable p_accomp
-    #p_accomp, key = accomp_gen.generate_accompaniment(chord_progression, major, note_duration)
+    p_accomp, key = accomp_gen.generate_accompaniment(chord_progression, major, accomp_rhythm)
 
     # Generate rhythm for melody
     melody_rhythm = rhythm_gen.generate_note_duration(chord_progression)
     # Generate a melody and assign the value to variable p_melody
-    p_melody = melody_gen.generate_melody(chord_progression, melody_rhythm)
+    p_melody = melody_gen.generate_melody(chord_progression, melody_rhythm, major, key)
     # Add the melody part to the stream
     stream.append(p_melody)
     # Add the accompaniment part to the stream
-    #stream.append(p_accomp)
+    stream.append(p_accomp)
     # Show the stream in MusicXML format
     stream.show()
 
