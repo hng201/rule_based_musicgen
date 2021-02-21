cb6a405e926ba1a9a7279acd1f8c47f783cff8c0
hng201 <55842986+hng201@users.noreply.github.com>
2021-02-21 16:06:51 +0000

    Updated parameter names

--- musicparam.py
+++ musicparam.py
@@ -2,12 +2,13 @@ class MusicParam:
     def __init__(self):
         # Major Keys: 0 = C, 1 = G, 2 = D, 3 = A
         # Minor Keys: 4 = A, 5 = E, 6 = B, 7 = F#
-        self.key = 5
+        self.key = 7
         # Set minimum no of bars for chord progression generation
         self.min_no_bar = 12
-        # Set maximum number of rests that can be present in each bar
-        # This refers to rest notes and not note duration
+        # Set total duration for rests in the composition
+        # This refers to maximum duration of all rests counted
+        # within the composition
         # Set rest limit for accompaniment
-        self.accomp_rest_bar_limit = 2
+        self.accomp_rest_limit = 13.25
         # Set rest limit for melody
-        self.melody_rest_bar_limit = 1
+        self.melody_rest_limit = 6.75
