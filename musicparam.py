92c85438ee95d10b834a79db66002e280800d327
hng201 <hng201@caledonian.ac.uk>
2021-03-07 20:39:48 +0000

    Added music param accomp_type for selecting accompaniment type

--- musicparam.py
+++ musicparam.py
@@ -2,7 +2,7 @@ class MusicParam:
     def __init__(self):
         # Major Keys: 0 = C, 1 = G, 2 = D, 3 = A
         # Minor Keys: 4 = A, 5 = E, 6 = B, 7 = F#
-        self.key = 7
+        self.key = 5
         # Set minimum no of bars for chord progression generation
         self.min_no_bar = 12
         # Set rest limit per bar
@@ -11,4 +11,7 @@ class MusicParam:
         # Set rest limit for accompaniment
         self.accomp_rest_limit = 1.5
         # Set rest limit for melody
-        self.melody_rest_limit = 0.5
+        self.melody_rest_limit = 1
+        # Set accompaniment type
+        # 0 = chord based, 1 = arpeggio based, 2 = chord and arpeggio
+        self.accomp_type = 1
