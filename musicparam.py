022019e8dff90c447722f4a0032b276c2d27db54
hng201 <hng201@caledonian.ac.uk>
2021-03-09 11:19:24 +0000

    Updated comment

--- musicparam.py
+++ musicparam.py
@@ -2,11 +2,11 @@ class MusicParam:
     def __init__(self):
         # Major Keys: 0 = C, 1 = G, 2 = D, 3 = A
         # Minor Keys: 4 = A, 5 = E, 6 = B, 7 = F#
-        self.key = 5
+        self.key = 6
         # Set minimum no of bars for chord progression generation
         self.min_no_bar = 12
         # Set rest limit per bar
-        # This refers to total duration of rest notes
+        # This refers to total duration of rest notes in a single bar
         # Max is 4 due to each bar being 4 beats
         # Set rest limit for accompaniment
         self.accomp_rest_limit = 1.5
@@ -14,4 +14,4 @@ class MusicParam:
         self.melody_rest_limit = 1
         # Set accompaniment type
         # 0 = chord based, 1 = arpeggio based, 2 = chord and arpeggio
-        self.accomp_type = 1
+        self.accomp_type = 2
